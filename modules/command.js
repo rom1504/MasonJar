bd554941d47d1f93dac9268dd382f7fefcdf8ea2
wski <contact.mattdylan@gmail.com>
2015-11-06 17:10:41 -0500

    feat(Command Dispatcher): dispatches known commands

--- modules/command.js
+++ modules/command.js
@@ -5,13 +5,22 @@ var commands = {
 };
 
 const command = function(mc, payload, callback) {
-  console.log(`${payload.player} dispatched command: ${payload.command}`);
-  mc.writeServer(`say ${payload.player} dispatched command: ${payload.command}`);
 
   var commandName = payload.command.split('(')[0].replace('$', '');
   var args = payload.command.split('(')[1].replace(')', '');
 
-  commands[commandName](JSON.parse(args));
+  if(commandName in commands) {
+
+    commands[commandName](JSON.parse(args));
+
+    console.log(`${payload.player} dispatched command: ${payload.command}`);
+    mc.writeServer(`say ${payload.player} dispatched command: ${payload.command}`);
+
+  }else {
+
+    console.log(`${payload.player} tried to dispatch an unknown command: ${payload.command}`);
+    
+  }
 
   callback({error: false});
 };
