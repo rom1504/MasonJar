de2af154f1450574a10c74d3e500d7f2bcc73d94
wski <contact.mattdylan@gmail.com>
2015-11-06 23:33:56 -0500

    feat(help): added ()

--- modules/commands/index.js
+++ modules/commands/index.js
@@ -1,9 +1,11 @@
 var vote_to_ban = require('./vote_to_ban.js');
 var vote_to_unban = require('./vote_to_unban.js');
 var starter_pack = require('./starter_pack.js');
+var help = require('./help.js');
 
 module.exports = {
   vote_to_ban,
   vote_to_unban,
-  starter_pack
+  starter_pack,
+  help
 };
--- modules/index.js
+++ modules/index.js
@@ -1,11 +1,9 @@
 var cleanup = require('./cleanup.js');
 var mcParser = require('./mcParser.js');
 var command = require('./command.js');
-var help = require('./help.js');
 
 module.exports = {
   cleanup,
   mcParser,
-  command,
-  help
+  command
 };
